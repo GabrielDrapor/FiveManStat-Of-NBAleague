919cd67b6603d1f8aaa4d8ed71bb5f732345eacf
GabrielDrapor <diary.sjr@gmail.com>
2017-03-28 17:28:08 +0800

    add the output part

--- MIN_Std_Deviation/MIN_Std_Deviation.py
+++ MIN_Std_Deviation/MIN_Std_Deviation.py
@@ -6,15 +6,17 @@ Description:
 Calculate standard deviation of minutes played of each team of NBA 's lineups
 
 """
+
 import re
 import urllib2
-
+import datetime
+import sys
 
 url='https://nba.hupu.com/teams/'
 url_team=''
 pattern = r'(?<=https://nba.hupu.com/teams/).*?(?=")'
 pattern_time = r'(?<=<span class="c5">).*?(?=</span>)'
-
+teampattern = r'(?<="name":").*?(?=")'
                  
 request = urllib2.Request(url)
 response = urllib2.urlopen(request)
@@ -24,9 +26,16 @@ teamlist=re.findall(pattern,content)
 
 teamlist={}.fromkeys(teamlist).keys()
 teamlist.sort()
+
+
+dict2={}
+
 idx=2
 t=1
 teamdict={}
+
+
+print 'Crawling data...'
 for team in teamlist:
     print t,'/30...'
     t+=1
@@ -34,6 +43,7 @@ for team in teamlist:
     avg_time=0.0
     variance =0.0
     url_team = url+team
+    
     subrequest = urllib2.Request(url_team)
     subresponse = urllib2.urlopen(subrequest)
     subcontent = subresponse.read()
@@ -56,5 +66,37 @@ for team in teamlist:
     variance/=q
     teamdict[team]=round(variance**(0.5),3)
 
-print 'Finished!'
+print 'Crawling Finished!'
 dict= sorted(teamdict.iteritems(), key=lambda d:d[1], reverse = False)
+
+teamlist.pop(0)
+teamlist.remove('mavericks')
+teamlist.insert(0,'sixers')
+
+gurl="http://www.nba.com/"
+fullname=[]
+print 'Loading the full name of each teams...'
+
+
+for team in teamlist:
+    sys.stdout.write('#')
+    request=urllib2.Request(gurl+team)
+    response=urllib2.urlopen(request)
+    content=response.read()
+    fullname.append(re.findall(teampattern,content)[0])
+    
+fullname.insert(16,'Dallas Mavericks')
+
+teamlist.insert(16,'mavericks')
+teamlist.pop(0)
+teamlist.insert(0,'76ers')
+
+for i in range(30):
+    dict2[teamlist[i]]=fullname[i]
+
+
+print 'Up to',datetime.date.today().strftime("%Y-%m-%d"),',the rank is:'
+
+for i in range(30):
+    print '\t',dict2[dict[i][0]]
+
--- MIN_Std_Deviation/readme.md
+++ MIN_Std_Deviation/readme.md
@@ -4,5 +4,38 @@ Recently, I was wondering which team in NBA League has the most balanced usage o
 
 To my surprise, the team with the smallest std deviation is not SanAntonio Spurs but Brooklyn Nets. And in front of the list, most teams are "lottery team", that sounds reasonable.
 
+The code is still shitty, maybe I'll fix it someday.
 
+Up to 17/03/28, the rank of team is:
+
+> Brooklyn Nets
+> Sacramento Kings
+> Chicago Bulls
+> Los Angeles Lakers
+> Phoenix Suns
+> Memphis Grizzlies
+> San Antonio Spurs
+> Atlanta Hawks
+> Philadelphia 76ers
+> Utah Jazz
+> Oklahoma City Thunder
+> Orlando Magic
+> Toronto Raptors
+> New York Knicks
+> Milwaukee Bucks
+> Golden State Warriors
+> Los Angeles Clippers
+> Denver Nuggets
+> Cleveland Cavaliers
+> Miami Heat
+> Indiana Pacers
+> Charlotte Hornets
+> Boston Celtics
+> Detroit Pistons
+> Dallas Mavericks
+> New Orleans Pelicans
+> Washington Wizards
+> Portland Trail Blazers
+> Minnesota Timberwolves
+> Houston Rockets
 
